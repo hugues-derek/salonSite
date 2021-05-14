3a604c120a2dfec1135bdd0a316cabd2ca8c07b9
hugues-derek <derekkouakou05@gmail.com>
2021-05-14 07:21:36 +0200

    dernier commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menuBtn = document.getElementById("menuBtn");
 const sideNav = document.getElementById("sideNav");
 const menu = document.getElementById("menu");
+const liens = document.querySelectorAll("#sideNav nav ul li a");
+const logo = document.querySelector(".logo");
+const para = document.querySelector(".banner-text p");
+const title = document.querySelector(".banner-text h1");
+const buttons = document.querySelectorAll(".banner-btn a");
+
 sideNav.style.right = "-250px";
 
 menuBtn.onclick = function () {
@@ -12,3 +18,45 @@ menuBtn.onclick = function () {
 		menu.src = "./Barber_Shop_img/menu.png";
 	}
 };
+for (let elt of liens) {
+	elt.onclick = () => {
+		if ((sideNav.style.right = "0")) {
+			sideNav.style.right = "-250px";
+			sideNav.style.transition = "1s";
+			menu.src = "./Barber_Shop_img/menu.png";
+		}
+	};
+}
+
+//Pour Controler le scroll de la page
+const scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500,
+	speedDuration: true,
+});
+
+//Pour les animations
+
+const TL1 = new gsap.timeline({ paused: false });
+
+TL1.from(title, 3, { scale: 0, x: -100 }, "+=1")
+	.from(
+		logo,
+		1,
+		{
+			opacity: 0,
+			y: -100,
+		},
+		"-=1"
+	)
+	.from(para, 1, { opacity: 0, x: -100 }, "-=0.5")
+	.from(
+		menuBtn,
+		1,
+		{
+			opacity: 0,
+			y: -100,
+		},
+		"-=1"
+	)
+	.from(buttons[0], 2, { opacity: 0, x: -200 }, "+= 1")
+	.from(buttons[1], 2, { opacity: 0, x: 200 }, "+= 1");
